f5a4e46ec980eedc658ebce6f8838a39e6d015e3
Lena <elena.pokhvalennaya@gmail.com>
2022-02-06 13:06:53 +0200

    added index.html and main.js files

---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// task1
+
+let name;
+
+name = prompt("What is your name?");
+alert(name);
+
+// task2
+
+let o = 8;
+let r = 3;
+
+let sumResult = o + r;
+alert(o + " + " + r + " = " + sumResult);
+
+let subResult = o - r;
+alert(o + " - " + r + " = " + subResult);
+
+let mulResult = o * r;
+alert(o + " * " + r + " = " + mulResult);
+
+let divResult = o / r;
+alert(o + " / " + r + " = " + divResult);
+
+let modResult = o % r;
+alert(o + " % " + r + " = " + modResult);
+
+let expResult = o ** r;
+alert(o + " ** " + r + " = " + expResult);
